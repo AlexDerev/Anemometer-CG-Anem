9de7b5d955718fae1cce2e73578c691de4a32b9a
AlexDerev <31729174+AlexDerev@users.noreply.github.com>
2023-02-10 01:25:59 +0300

    Rearranged main screen
    Облагорожен основной экран, выведены верхние и нижние границы, переставлены значения и пр.

--- Firmware/AnemCode/src/main.cpp
+++ Firmware/AnemCode/src/main.cpp
@@ -7,7 +7,10 @@
 #include "FlowPlot.cpp"
 
 #define ADC_pin A0 // задаём значение пина АЦП
-#define PLOT_COUNTS 60 //количество отсчетов на графике
+#define PLOT_LEFT   67  // левая граница графика
+#define PLOT_TOP    10  // левая граница графика
+#define PLOT_HEIGHT 50  // высота графика
+#define PLOT_COUNTS 60  //количество отсчетов на графике
 
 GyverOLED<SSH1106_128x64> oled; // Инициализируем OLED-экран
 CG_Anem cgAnem(ANEM_I2C_ADDR); // Инициализируем CG_Anem
@@ -17,7 +20,6 @@ uint16_t ADC; // Переменная для значений АЦП
 uint32_t timer_cnt; // Таймер для измерений анемометра
 uint32_t timer_bat; // Таймер для измерения заряда батареи
 const uint8_t *p;
-uint8_t height = 40;
 float flow;
 
 void setup() {
@@ -54,28 +56,52 @@ void setup() {
   delay(1000);
   oled.clear();
   oled.update();
-  oled.setScale(1);
-  flowPlot.init(height);
+  flowPlot.init(PLOT_HEIGHT);
   p = flowPlot.getPoints();
 }
 
 void loop() {
   char buf[50];
+  oled.setScale(1);
   if (millis() - timer_cnt > 1000) { // Снимаем показания с анемометра и выводим их на экран
     timer_cnt = millis();
     // Проверяем, обновляются ли данные с анемометра. Если да - выводим их, если нет - предупреждаем об ошибке
     if (cgAnem.data_update()) {
       flow = cgAnem.getAirflowRate();
       flowPlot.pushValue(flow);
-      sprintf(buf, "V: %.1f m/s ", flow); // Собираем строку с показаниями скорости потока
-      oled.setCursor(0, 1);
+      sprintf(buf, "T:%.1fC ", cgAnem.getTemperature()); // Собираем строку с показаниями температуры
+      oled.setCursor(0, 0);
       oled.print(buf);
-      sprintf(buf, "T: %.1f C ", cgAnem.getTemperature()); // Собираем строку с показаниями температуры
+      
+      // отрисовка графика
+      oled.rect(PLOT_LEFT, PLOT_TOP, PLOT_LEFT + PLOT_COUNTS, PLOT_TOP + PLOT_HEIGHT, OLED_CLEAR); 
+      oled.rect(PLOT_LEFT, PLOT_TOP, PLOT_LEFT + PLOT_COUNTS, PLOT_TOP + PLOT_HEIGHT, OLED_STROKE);
+      for (uint8_t i = 0; i < PLOT_COUNTS; i++) {
+        oled.dot(PLOT_LEFT + i, PLOT_TOP + p[i], OLED_FILL);
+      }
+  
+      sprintf(buf, "%.1f", flowPlot.getMaxRange()); // Собираем строку со значением для верхней границы
+      oled.setCursorXY(PLOT_LEFT - 20, PLOT_TOP);
+      oled.print(buf);
+
+      sprintf(buf, "%.1f", flowPlot.getMinRange()); // Собираем строку со значением для нижней границы
+      oled.setCursorXY(PLOT_LEFT - 20, PLOT_TOP + PLOT_HEIGHT - 8);
+      oled.print(buf);
+
+      sprintf(buf, "M:%.1fm/s", flowPlot.getMeanFlow()); // Собираем строку со средним потоком
+      oled.setCursor(55, 0);
+      oled.print(buf);
+
+      oled.setCursor(45, 4);
+      oled.println("m/s");
+
+      sprintf(buf, "%.1f", flow); // Собираем строку с показаниями скорости потока
       oled.setCursor(0, 3);
+      oled.setScale(2);
       oled.print(buf);
     }
     else {
-      oled.setCursor(45, 3);
+      oled.setCursor(35, 0);
       oled.print("*");
     }
   }
@@ -83,26 +109,21 @@ void loop() {
   if (millis() - timer_bat > 10000) { //
     timer_bat = millis();
     ADC = analogRead(ADC_pin); // Считываем показание с АЦП
-    oled.rect(104, 3, 124, 10, OLED_STROKE); // Рисуем иконку батарейки
-    oled.rect(125, 5, 127, 8, OLED_FILL);
+    oled.rect(112, 0, 124, 3, OLED_STROKE); // Рисуем иконку батарейки
+    oled.rect(125, 1, 127, 2, OLED_FILL);
     if (ADC >= 970) {
-      oled.rect(104, 3, 124, 10, OLED_FILL);
+      oled.rect(112, 0, 124, 3, OLED_FILL);
     }
     if (ADC < 970 && ADC >= 870) {
-      oled.rect(106, 3, 119, 10, OLED_FILL);
+      oled.rect(112, 0, 121, 3, OLED_FILL);
     }
     if (ADC < 870 && ADC >= 770) {
-      oled.rect(106, 3, 114, 10, OLED_FILL);
+      oled.rect(112, 0, 118, 3, OLED_FILL);
+    }
+    if (ADC < 770 && ADC >= 670) {
+      oled.rect(112, 0, 115, 3, OLED_FILL);
     }
   }
 
-  oled.rect(60, 10, 60 + PLOT_COUNTS, 10 + height, OLED_CLEAR);
-  oled.rect(60, 10, 60 + PLOT_COUNTS, 10 + height, OLED_STROKE);
-  for (uint8_t i = 0; i < PLOT_COUNTS; i++) {
-    oled.dot(60 + i, 10 + p[i], OLED_FILL);
-  }
-  sprintf(buf, "M: %.1f m/s ", flowPlot.getMeanFlow()); // Собираем строку с показаниями температуры
-  oled.setCursor(0, 5);
-  oled.print(buf);
   oled.update();
 }
